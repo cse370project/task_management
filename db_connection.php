9cac1ae021accdfac930ffef78bc6cbfb91e16d1
ehteshamulhaqueadit <ehteshamadit@gmail.com>
2025-03-27 20:13:08 +0600

    Testing

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";  // Server name (usually localhost for local development)
 $username = "tm_admin";         // Your MySQL username
 $password = "tmadmin1234";            
-$dbname = "task_management"; 
+$dbname = "task_management"; // new newnewee
 
 
 $conn = new mysqli(hostname: $servername, username: $username, password: $password, database: $dbname);
